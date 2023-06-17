c2533aab98e2abc08eda36e0029346afead52785
George T. C. Lai <tsungchih.hd@gmail.com>
2023-06-17 15:17:20 +0800

    refactor(io_managers): migrate parquet_io_manager to new resource def
    migrate parquet_io_manager to new resource definition

--- medallion-air/medallion_air/resources/parquet_io_manager.py
+++ medallion-air/medallion_air/resources/parquet_io_manager.py
@@ -1,84 +1,87 @@
-from pathlib import Path
-from typing import Any, Union
+import os
+from typing import Union
 
-import pandas as pd
-from dagster import Field, InputContext, IOManager, OutputContext
-from dagster import _check as check
-from dagster import io_manager
+import pandas
 from dagster._seven.temp_dir import get_system_temp_directory
+from dagster_pyspark.resources import PySparkResource
+from pyspark.sql import DataFrame as PySparkDataFrame
 
-from .utils import get_output_metadata_entries
+from dagster import (
+    ConfigurableIOManager,
+    InputContext,
+    OutputContext,
+    ResourceDependency,
+)
+from dagster import _check as check
 
 
-class PartitionedParquetIOManager(IOManager):
-    """
-    This IOManager will take in a Pandas DataFrame and store it in parquet at the
+class PartitionedParquetIOManager(ConfigurableIOManager):
+    """This IOManager will take in a pandas or pyspark dataframe and store it in parquet at the
     specified path.
 
-    It stores outputs for different partitions in different file paths.
+    It stores outputs for different partitions in different filepaths.
 
-    Downstream ops can either load this DataFrame into a Pandas DataFrame or simply retrieve a path
+    Downstream ops can either load this dataframe into a spark session or simply retrieve a path
     to where the data is stored.
     """
 
-    def __init__(self, base_path: str, load_as_dataframe: bool = True):
-        self._base_path = base_path
-        self._load_as_dataframe = load_as_dataframe
+    pyspark: ResourceDependency[PySparkResource]
 
-    def handle_output(self, context: OutputContext, obj: pd.DataFrame):
+    @property
+    def _base_path(self):
+        raise NotImplementedError()
+
+    def handle_output(self, context: OutputContext, obj: Union[pandas.DataFrame, PySparkDataFrame]):
         path = self._get_path(context)
-        parent_path = path.parent
         if "://" not in self._base_path:
-            parent_path.mkdir(mode=0o755, parents=True, exist_ok=True)
+            os.makedirs(os.path.dirname(path), exist_ok=True)
 
-        if isinstance(obj, pd.DataFrame):
+        if isinstance(obj, pandas.DataFrame):
             row_count = len(obj)
             context.log.info(f"Row count: {row_count}")
-            obj.to_parquet(path=path.as_posix(), index=False, compression="snappy")
+            obj.to_parquet(path=path, index=False)
+        elif isinstance(obj, PySparkDataFrame):
+            row_count = obj.count()
+            obj.write.parquet(path=path, mode="overwrite")
         else:
             raise Exception(f"Outputs of type {type(obj)} not supported.")
 
-        context.add_output_metadata({"row_count": row_count, "path": path.as_posix()})
+        context.add_output_metadata({"row_count": row_count, "path": path})
 
-    def load_input(self, context: InputContext) -> Union[pd.DataFrame, str]:
+    def load_input(self, context) -> Union[PySparkDataFrame, str]:
         path = self._get_path(context)
-        if self._load_as_dataframe:
-            return pd.read_parquet(path=path.as_posix())
-        else:
-            metadata_entries = get_output_metadata_entries(
-                entry_keys=["path"], ctx=context.upstream_output
-            )
-            return metadata_entries["path"]
+        if context.dagster_type.typing_type == PySparkDataFrame:
+            # return pyspark dataframe
+            return self.pyspark.spark_session.read.parquet(path)
 
-    def _get_path(self, context: Union[InputContext, OutputContext]) -> Path:
+        return check.failed(
+            f"Inputs of type {context.dagster_type} not supported. Please specify a valid type "
+            "for this input either on the argument of the @asset-decorated function."
+        )
+
+    def _get_path(self, context: Union[InputContext, OutputContext]):
         key = context.asset_key.path[-1]
 
         if context.has_asset_partitions:
             start, end = context.asset_partitions_time_window
             dt_format = "%Y%m%d%H%M%S"
             partition_str = start.strftime(dt_format) + "_" + end.strftime(dt_format)
-            return Path(self._base_path).joinpath(Path(key), Path(f"{partition_str}.parquet"))
+            return os.path.join(self._base_path, key, f"{partition_str}.pq")
         else:
-            return Path(self._base_path).joinpath(Path(f"{key}.parquet"))
+            return os.path.join(self._base_path, f"{key}.pq")
 
 
-@io_manager(
-    config_schema={
-        "base_path": Field(
-            str, description="The base path for store parquet files.", is_required=False
-        ),
-        "load_as_dataframe": Field(
-            bool,
-            description="""Indicates whether to load data as a Pandas DataFrame, defaults to True."
-            Path to the parquet file will be returned if the value is False.
-            """,
-            default_value=True,
-            is_required=False,
-        ),
-    },
-)
-def local_partitioned_parquet_io_manager(init_context):
-    return PartitionedParquetIOManager(
-        base_path=init_context.resource_config.get("base_path", get_system_temp_directory()),
-        load_as_dataframe=init_context.resource_config.get("load_as_dataframe"),
-    )
+class LocalPartitionedParquetIOManager(PartitionedParquetIOManager):
+    base_path: str = get_system_temp_directory()
+
+    @property
+    def _base_path(self):
+        return self.base_path
+
+
+class S3PartitionedParquetIOManager(PartitionedParquetIOManager):
+    s3_bucket: str
+
+    @property
+    def _base_path(self):
+        return "s3://" + self.s3_bucket
