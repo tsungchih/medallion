f8674c5855c8afb0abc3325de8089a48d531e3bf
George T. C. Lai <tsungchih.hd@gmail.com>
2023-06-17 15:23:56 +0800

    refactor(resources): update to all Pythonic resources definition
    update all the used resources to their corresponding Pythonic resource
    definitions

--- medallion-air/medallion_air/resources/__init__.py
+++ medallion-air/medallion_air/resources/__init__.py
@@ -1,31 +1,23 @@
-from dagster_gcp.gcs import gcs_pickle_io_manager, gcs_resource
+from dagster_gcp.gcs import ConfigurablePickledObjectGCSIOManager, GCSResource
 
-from .gcs_client import gcs_client
-from .parquet_io_manager import local_partitioned_parquet_io_manager
+from dagster import EnvVar
+
+from .gcs_client import GCSClient
 
 RESOURCES_DEV = {
-    "gcs": gcs_resource,
-    "gcs_io_manager": gcs_pickle_io_manager.configured(
-        {
-            "gcs_bucket": "dagster-demo-iomanager",
-            "gcs_prefix": "dagster",
-        }
+    "gcs_io_manager": ConfigurablePickledObjectGCSIOManager(
+        gcs=GCSResource(), gcs_bucket="dagster-demo-iomanager", gcs_prefix="dagster"
     ),
-    "gcs_client": gcs_client.configured(
-        {
-            "project_id": {"env": "GOOGLE_CLOUD_PROJECT"},
-            "credentials": {"env": "GOOGLE_APPLICATION_CREDENTIALS"},
-        }
+    "gcs_client": GCSClient(
+        project_id=EnvVar("GOOGLE_CLOUD_PROJECT"),
+        credentials=EnvVar("GOOGLE_APPLICATION_CREDENTIALS"),
     ),
-    "parquet_io_manager": local_partitioned_parquet_io_manager,
 }
 
 RESOURCES_PROD = {
-    "gcs_client": gcs_client.configured(
-        {
-            "project_id": {"env": "GOOGLE_CLOUD_PROJECT"},
-            "credentials": {"env": "GOOGLE_APPLICATION_CREDENTIALS"},
-        }
+    "gcs_client": GCSClient(
+        project_id=EnvVar("GOOGLE_CLOUD_PROJECT"),
+        credentials=EnvVar("GOOGLE_APPLICATION_CREDENTIALS"),
     )
 }
 
